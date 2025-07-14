bfdcaac3dd5b45ec8c95b71bb987e27e685738a7
32067002d11 <Zahidkhanbusiness01@gmail.com>
2025-07-14 01:40:20 -0600

    Create App.jsx

---
+++ public/src/src/src/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Black Diamond</h1>
+      <p>This is your Amazon product research tool!</p>
+    </div>
+  );
+}
+
+export default App;
